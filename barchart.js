86cd3d1f73c6c47c18f405aa0f71138a94b14c07
sophieclaire <sophie.stiekema@gmail.com>
2019-06-07 22:26:51 +0200

    button

--- barchart.js
+++ barchart.js
@@ -2,11 +2,14 @@ window.onload = drawbarchart();
 
 function drawbarchart() {
 
-  fetch("data/IND.json")
+  fetch("data/BEL.json")
     .then(response => response.json())
     .then(nld => {
         console.log(nld)
-        console.log(nld.length)
+        nld.sort(function(a, b) {
+              return d3v5.ascending(a.Y2013, b.Y2013)
+            })
+        console.log(nld)
         var dataset = {}
 
         for (i = 0; i < nld.length; i ++) {
@@ -15,7 +18,7 @@ function drawbarchart() {
                      continue
                  }
                  else {
-                     dataset[nld[i].Item] = nld[i].Y1961
+                     dataset[nld[i].Item] = nld[i].Y2013
                  };
              }
         }
@@ -37,9 +40,9 @@ function bar(dataset) {
 
 
   // set dimensions
-  var margin = {top: 70, right: 20, bottom: 120, left: 200},
+  var margin = {top: 90, right: 20, bottom: 120, left: 200},
       w = 900 - margin.left - margin.right,
-      h = 950 - margin.top - margin.bottom,
+      h = 1100 - margin.top - margin.bottom,
       barPadding = 1;
 
   var minValue = Math.min.apply(null, Object.values(dataset)),
@@ -71,7 +74,7 @@ for (i = 0; i < dataset.length; i++){
       .attr('class', 'd3-tip')
       .offset([-10, 0])
       .html(function(d) {
-        return "<strong>Index:</strong> <span style='color:lavender'>" + d + "</span>";
+        return "<strong>Amount:</strong> <span style='color:lavender'>" + d + "</span>";
         })
 
   //create SVG element
@@ -130,7 +133,7 @@ for (i = 0; i < dataset.length; i++){
         .attr("dy", "1em")
         .style("text-anchor", "middle")
         .style("fill", "black")
-        .text("Production amount");
+        .text("Food produced");
 
     // add x-axis
     svg.append("g")
@@ -141,7 +144,15 @@ for (i = 0; i < dataset.length; i++){
       .style("text-anchor", "end")
       .attr("dx", "-.8em")
       .attr("dy", "-.55em")
-      .attr("transform", "rotate(-90)" );
+      .attr("transform", "rotate(-90)" )
+     .append("text")
+        .attr("class", "x label")
+        .attr("y", 0 - 2 * margin.left )
+        .attr("x", 0 - h /2 )
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .style("fill", "black")
+        .text("Amount produced in tonnes");
 
     // add title
     svg.append("text")
@@ -150,7 +161,7 @@ for (i = 0; i < dataset.length; i++){
         .attr("text-anchor", "middle")
         .style("font-size", "16px")
         .style("text-decoration", "underline")
-        .style("font-style", "italic")
+        .style("font-style", "bold")
         .text("Food categories");
 
 }
